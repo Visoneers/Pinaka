7f36f5f00b22820fb6f5b58bb1d2da90fb31a07e
Prasun Bhunia <bhunia.prasun99@gmail.com>
2020-11-02 17:27:38 +0530

    Products add to store

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name=models.CharField(max_length=200,null=True)
     price=models.FloatField()
     digital=models.BooleanField(default=False,null=True,blank=False)
-    #image
+    image=models.ImageField(null=True,blank=True)
 
     def __str__(self):       
         return self.name
