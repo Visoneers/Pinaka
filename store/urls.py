68a69c7802f240e93e4ae41fab3858f2efb6cf6d
ROHITDIVEKAR <rohitdivekar463@gmail.com>
2020-11-05 19:05:28 +0530

    added dynamic image urls n cart section done

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
     path('', views.store, name = "store"),
     path('cart/', views.cart, name = "cart"),
     path('checkout/', views.checkout, name = "checkout"),
 ]
+
+urlpatterns+= static(settings.MEDIA_URL , document_roots=settings.MEDIA_ROOT) #chk
