10d69cc06d24463cedd5930e1bd076b9f4f17d39
ROHITDIVEKAR <rohitdivekar463@gmail.com>
2020-11-08 12:18:42 +0530

    chk kar bhai

--- store/views.py
+++ store/views.py
@@ -79,32 +79,33 @@ def updateItem(request):
 
 	return JsonResponse('Item was added', safe=False)
 
+
 def processOrder(request):
-    # print('date:', request.body)
-	transaction_id = datetime.datetime.now().timestamp()
-	data = json.loads(request.body)
+    print('date:', request.body)
+# 	transaction_id = datetime.datetime.now().timestamp()
+# 	data = json.loads(request.body)
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
+# 	if request.user.is_authenticated:
+# 		customer = request.user.customer
+# 		order,created = Order.objects.get_or_create(customer=customer, complete=False)
+#         total = float(data['form']['total'])
+#         order.transaction_id = transaction_id
         
-        if total == order.get_cart_total:
-		    order.complete = True
-	    Order.save()
+#         if total == order.get_cart_total:
+# 		    order.complete = True
+# 	    Order.save()
 
-        if order.shipping == True:
-		    ShippingAddress.objects.create(
-		        customer=customer,
-		        order=order,
-		        address=data['shipping']['address'],
-		        city=data['shipping']['city'],
-		        state=data['shipping']['state'],
-		        zipcode=data['shipping']['zipcode'],
-		    )
-	else:
-        print('user not logged in...')
-        # customer, order = guestOrder(request, data)
+#         if order.shipping == True:
+# 		    ShippingAddress.objects.create(
+# 		        customer=customer,
+# 		        order=order,
+# 		        address=data['shipping']['address'],
+# 		        city=data['shipping']['city'],
+# 		        state=data['shipping']['state'],
+# 		        zipcode=data['shipping']['zipcode'],
+# 		    )
+# 	else:
+#         print('user not logged in...')
+#         # customer, order = guestOrder(request, data)
 
     return JsonResponse('Payment submitted..', safe=False)
