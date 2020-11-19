c23c85de7b76f3eccdb8604ce72bf8a3809bf2d8
Sushant Kulkarni <Wanderer04@.github.com>
2020-11-19 17:04:04 +0530

    my part changes
    more to come

--- static/js/cart.js
+++ static/js/cart.js
@@ -17,6 +17,34 @@ for (var i=0;i<updateBtns.length; i++){
         
     })
 }
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
+}
+
+
 function updateUserOrder(productId, action){
 	    console.log('User is authenticated, sending data...')
 
