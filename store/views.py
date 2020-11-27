c97d6ee1f08bf84feb096a203052449a4ac721b7
Prasun Bhunia <bhunia.prasun99@gmail.com>
2020-11-27 11:55:59 +0530

    bugs fixed

--- store/views.py
+++ store/views.py
@@ -11,7 +11,7 @@ def store(request):
     if request.user.is_authenticated:
         customer=request.user.customer
         order,created=Order.objects.get_or_create(customer=customer,complete=False)
-        items=order.order.get_cart_items
+        items=order.get_cart_items
     else:
         items=[]
         order={'get_cart_total':0,'get_cart_items':0,'shipping':False}
